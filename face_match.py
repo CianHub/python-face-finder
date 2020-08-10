a056e25c60df053d0038da395ded2c6f6a8a7a6d
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-10 21:19:14 +0100

    Add face match code

--- face_match.py
+++ face_match.py
@@ -2,5 +2,8 @@ import face_recognition
 
 image_of_conor = face_recognition.load_image_file(
     './img/known/conor-mcgregor.jpeg')
+conor_face_encoding = face_recognition.face_encodings(image_of_conor)[0]
 
-conor_face_encoding = face_recognition.face_encodings(image_of_conor)
+unknown_image = face_recognition.load_image_file(
+    './img/unknown/1.jpg')
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
