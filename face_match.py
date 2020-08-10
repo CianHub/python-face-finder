216bb43cccdb3193419b1ded4ef75493dfa10b54
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-10 21:30:14 +0100

    Add functionality for matching two images

--- face_match.py
+++ face_match.py
@@ -1,9 +1,17 @@
 import face_recognition
 
-image_of_conor = face_recognition.load_image_file(
-    './img/known/conor-mcgregor.jpeg')
-conor_face_encoding = face_recognition.face_encodings(image_of_conor)[0]
+image_of_tenshin = face_recognition.load_image_file(
+    './img/known/tenshin-nasukawa.png')
+tenshin_face_encoding = face_recognition.face_encodings(image_of_tenshin)[0]
 
 unknown_image = face_recognition.load_image_file(
-    './img/unknown/1.jpg')
+    './img/unknown/2.jpeg')
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces(
+    [tenshin_face_encoding], unknown_face_encoding)
+
+if results[0]:
+    print('Its Tenshin')
+else:
+    print('It is not Tenshin')
